4ffa04a950512258524e78f886611a148cf465b9
DavidZaba <161326189+DavidZaba@users.noreply.github.com>
2024-11-29 11:00:17 -0500

    Update Reserva.js

--- models/Reserva.js
+++ models/Reserva.js
@@ -7,28 +7,22 @@ const mongoose = require('mongoose');
 const reservaSchema = new mongoose.Schema({
   numReserva: {
     type: String,
-    required: [true, 'El número de reserva es obligatorio']
   },
   cliente: {
     type: String,
-    required: [true, 'El nombre del cliente es obligatorio']
   },
   apellido: {
     type: String,
-    required: [true, 'El apellido del cliente es obligatorio']
   },
   tipoDoc: {
     type: String,
     enum: ['CC', 'TI', 'PP'],
-    required: [true, 'El tipo de documento es obligatorio']
   },
   numDoc: {
     type: String,
-    required: [true, 'El número de documento es obligatorio']
   },
   telefono: {
     type: String,
-    required: [true, 'El teléfono es obligatorio'],
     validate: {
       validator: function (v) {
         return /\d{10}/.test(v); // Validar que tenga 10 dígitos
@@ -38,7 +32,6 @@ const reservaSchema = new mongoose.Schema({
   },
   correo: {
     type: String,
-    required: [true, 'El correo es obligatorio'],
     validate: {
       validator: function (v) {
         return /\S+@\S+\.\S+/.test(v); // Validar formato de correo
@@ -48,7 +41,6 @@ const reservaSchema = new mongoose.Schema({
   },
   acompañantes: {
     type: Number,
-    required: [true, 'El número de acompañantes es obligatorio'],
     validate: {
       validator: function (v) {
         return v >= 0 && v <= 3; // Validar que no haya más de 3 acompañantes
@@ -58,7 +50,6 @@ const reservaSchema = new mongoose.Schema({
   },
   fInicio: {
     type: Date,
-    required: [true, 'La fecha de inicio es obligatoria'],
     validate: {
       validator: function (v) {
         return v > new Date(); // Validar que la fecha de inicio sea futura
@@ -68,7 +59,6 @@ const reservaSchema = new mongoose.Schema({
   },
   fFin: {
     type: Date,
-    required: [true, 'La fecha de fin es obligatoria'],
     validate: {
       validator: function (v) {
         return v > this.fInicio; // Validar que la fecha de fin sea posterior a la de inicio
@@ -79,17 +69,14 @@ const reservaSchema = new mongoose.Schema({
   tipoApto: {
     type: String,
     enum: ['tipo1', 'tipo2', 'Penthouse'],
-    required: [true, 'El tipo de apartamento es obligatorio']
   },
   total: {
     type: Number,
-    required: [true, 'El total es obligatorio'],
     min: [0, 'El total no puede ser negativo'] // Validar que el total no sea negativo
   },
   estado: {
     type: String,
     enum: ['confirmado', 'pendiente', 'cancelado'],
-    required: [true, 'El estado es obligatorio']
   }
 });
 
